1d7645a538c76f86abde52ba893d30d1b83eeaed
xuexb <fe.xiaowu@gmail.com>
2017-09-03 10:03:59 +0800

    添加mocha超时为6秒, 默认是3秒

--- karma.conf.base.js
+++ karma.conf.base.js
@@ -29,6 +29,13 @@ module.exports = config => {
         // list of files to exclude
         exclude: [],
 
+        client: {
+            mocha: {
+                // mocha测试超时6秒
+                timeout: 1000 * 6
+            }
+        },
+
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
