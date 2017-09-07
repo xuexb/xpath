cb19cfd60d5182e5b47680d879aaf73fa961f518
xuexb <fe.xiaowu@gmail.com>
2017-09-07 16:43:51 +0800

    测试bs

--- karma.conf.sauce.js
+++ karma.conf.sauce.js
@@ -8,63 +8,64 @@ const base = require('./karma.conf.base.js');
 const customLaunchers = {
     // Safari
     sl_ios_safari: {
-        base: 'SauceLabs',
-        browserName: 'Safari'
+        base: 'BrowserStack',
+        browser: 'safari',
+        os: 'ios'
     },
 
     // 安卓浏览器
-    sl_android_4_4: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        version: '4.4'
-    },
-    sl_android_5: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        version: '5'
-    },
-    sl_android_6: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        version: '6'
-    },
+    // sl_android_4_4: {
+    //     base: 'BrowserStack',
+    //     os: 'android',
+    //     os_version: '4.4'
+    // },
+    // sl_android_5: {
+    //     base: 'BrowserStack',
+    //     os: 'android',
+    //     os_version: '5'
+    // },
+    // sl_android_6: {
+    //     base: 'BrowserStack',
+    //     os: 'android',
+    //     os_version: '6'
+    // },
 
-    // chrome
-    sl_ios_chrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome'
-    },
+    // // chrome
+    // sl_ios_chrome: {
+    //     base: 'BrowserStack',
+    //     browser: 'chrome'
+    // },
 
     // sl_ie_8: {
-    //     base: 'SauceLabs',
-    //     browserName: 'internet explorer',
-    //     platform: 'Windows 7',
+    //     base: 'BrowserStack',
+    //     browser: 'internet explorer',
+    //     os: 'Windows 7',
     //     version: '8'
     // },
-    sl_ie_9: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9'
-    },
-    sl_ie_10: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8',
-        version: '10'
-    },
-    sl_ie_11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-    },
+    // sl_ie_9: {
+    //     base: 'BrowserStack',
+    //     browser: 'internet explorer',
+    //     os: 'Windows 7',
+    //     version: '9'
+    // },
+    // sl_ie_10: {
+    //     base: 'BrowserStack',
+    //     browser: 'internet explorer',
+    //     os: 'Windows 8',
+    //     version: '10'
+    // },
+    // sl_ie_11: {
+    //     base: 'BrowserStack',
+    //     browser: 'internet explorer',
+    //     os: 'Windows 8.1',
+    //     version: '11'
+    // },
 
-    sl_firefox: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 7'
-    }
+    // sl_firefox: {
+    //     base: 'BrowserStack',
+    //     browser: 'firefox',
+    //     os: 'Windows 7'
+    // }
 };
 
 // 不支持本地运行
@@ -74,27 +75,23 @@ if (!process.env.TRAVIS) {
 }
 
 // 变量检查
-if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+if (!process.env.BROWSER_STACK_USERNAME || !process.env.BROWSER_STACK_ACCESS_KEY) {
     console.error('---------------');
-    console.error('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+    console.error('Make sure the BROWSER_STACK_USERNAME and BROWSER_STACK_ACCESS_KEY environment variables are set.');
     console.error('---------------');
     process.exit(1);
 }
 
 module.exports = function (config) {
     const options = Object.assign(base(config), {
-        reporters: ['mocha', 'saucelabs'],
-        sauceLabs: {
-            'testName': 'xpath',
-            'recordVideo': false,
-            'recordScreenshots': false,
-            'startConnect': false,
-            'connectOptions': {
-                'no-ssl-bump-domains': 'all'
-            },
-            'public': 'public',
-            'build': 'xpath-build-' + process.env.TRAVIS_BUILD_NUMBER,
-            'tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER
+        reporters: ['mocha', 'browserStack'],
+        browserStack: {
+            name: 'xpath',
+            video: false,
+            startTunnel: false,
+            project: 'xpath',
+            build: 'xpath-build-' + process.env.TRAVIS_BUILD_NUMBER,
+            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
         },
         customLaunchers: customLaunchers,
         browsers: Object.keys(customLaunchers),
